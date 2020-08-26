c77dda39e78b8d1cbf5540f81598d5da2e98d07c
Sarcher01 <66928968+Sarcher01@users.noreply.github.com>
2020-08-25 23:06:47 -0400

    Project Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/********Page Animations*********/
+const banner = document.querySelector('.banner');
+const slider = document.querySelector('.slider');
+const banner-text = document.querySelector('.banner-text');
+
+const tl = new TimelineMax();
+
+tl.fromTo(banner, 1, {height:"0%"}, {height:"80%", ease: Power2.easeInOut })
+.fromTo(banner, 1.2, {width: "100%"}, {width:"80%", ease: Power2.easeInOut })
+
+.fromTo(slider, 1.2, {x: "-100%"}, {x:"0%", ease: Power2.easeInOut}, "-=1.2")
+
+.fromTo(banner-text, 0.5, { opacity: 0, x: 30 }, {opacity: 1, x:0 }, "-=0.5");
