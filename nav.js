bb7ca54a8ec39d4d82c5908b4df8c55c7fc68a38
Sarcher01 <66928968+Sarcher01@users.noreply.github.com>
2020-09-07 12:47:22 -0400

    Fixed toggle class

--- nav.js
+++ nav.js
@@ -1,6 +1,5 @@
-        $(document).ready(function(){
-            $('.menu-toggle').click(function(){
-                $('nav').toggleClass('active')
+$(document).ready(function(){
+    $('.menu-toggle').click(function(){
+         $('nav').toggleClass('active')
             })
         })
-
